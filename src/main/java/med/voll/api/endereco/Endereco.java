8c81db2982dc62e700a719a2ffaff0f1bc4cf1ef
Lucas Mendes <lucasMendesHQ@outlook.com>
2023-01-17 23:16:22 -0300

    Iniciando o cadastro de médicos com Spring Data JPA

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,12 @@
+package med.voll.api.endereco;
+
+public class Endereco {
+
+	private String logradouroString;
+	private String bairro;
+	private String cep;
+	private String numero;
+	private String complemento;
+	private String cidade;
+	private String uf;
+}
