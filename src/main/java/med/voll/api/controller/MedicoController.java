7dfb513867737e2ff5078b9ac683588d7d702942
Lucas Mendes <lucasMendesHQ@outlook.com>
2023-01-18 22:15:08 -0300

    Listagem de dados com DTO

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
 import java.security.PublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -22,6 +25,8 @@ public class MedicoController {
 	//RequestBody - requisição via POST que le o body do postman (Padrão json)
 	//Valid - valida os campos das DTOs
 	//PostMapping requisição via POST
+	// @Transactional = Alterações tais como deleção, alteração ou inserção
+	// repository - classe que acessa o banco de dados
 	
 	
 	@Autowired  
@@ -32,11 +37,21 @@ public class MedicoController {
 	@Transactional 
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
 		
-		
 		repository.save(new Medico(dados) );
 		
+	}
+	
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		
+		
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 		
 	}
+	
+	
+	
 	
 	
 }
